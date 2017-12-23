28b0f212e75bbc5db222c186915dc6d2ac14a719
bunnieabc <bunnieabc@gmail.com>
2017-12-23 03:54:51 -0500

    revise

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -28,8 +28,8 @@ var polygons = [
     //polygon: "50% 0%, 0% 100%, 100% 100%",
     polygon: "50% 0%, 100% 100%, 100% 100%, 50% 100%, 0% 100%, 0% 100%",
     background: "135deg, #68C4CA 0%, #68C4CA 25%, #68C4CA 77%, #68C4CA 100%",
-    width: "88",
-    height: "77",
+    width: "96",
+    height: "84",
     transform: "rotate(-25deg)"
   },
   {
@@ -37,8 +37,8 @@ var polygons = [
     //polygon: "50% 0%, 0% 100%, 100% 100%",
     polygon: "0% 0%, 100% 0%, 100% 100%, 100% 100%, 0% 100%, 0% 100%",
     background: "135deg, #FFE141 0%, #FFE141 25%, #FFE141 77%, #FFE141 100%",
-    width: "75",
-    height: "75",
+    width: "84",
+    height: "84",
     transform: "rotate(0deg)"
   },
   {
@@ -46,16 +46,16 @@ var polygons = [
     //polygon: "50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%",
     polygon: "50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%, 0% 38%",
     background: "135deg, #FF7373 0%, #FF7373 11%, #FF7373 79%, #FF7373 100%",
-    width: "84",
-    height: "80",
+    width: "105",
+    height: "100",
     transform: "rotate(45deg)"
   },
   {
     type: "hexagon",
     polygon: "50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%",
     background: "135deg, #569CF8 0%, #569CF8 5%, #569CF8 66%, #569CF8 100%",
-    width: "84",
-    height: "96",
+    width: "98",
+    height: "112",
     transform: "rotate(130deg)"
   }
 ]
