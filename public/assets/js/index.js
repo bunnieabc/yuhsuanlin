ae62d239de38c9f6313a1b1088f9c1aa637d428d
Yu-Hsuan Lin <bunnieabc@Yu-Hsuans-MBP.attlocal.net>
2021-05-02 20:38:37 -0700

    V2 WIP

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -68,8 +68,8 @@ var polygons = [
     //polygon: "50% 0%, 0% 100%, 100% 100%",
     polygon: "50% 0%, 100% 100%, 100% 100%, 50% 100%, 0% 100%, 0% 100%",
     background: "135deg, #68C4CA 0%, #68C4CA 25%, #68C4CA 77%, #68C4CA 100%",
-    width: "96",
-    height: "84",
+    width: "32",
+    height: "28",
     transform: "rotate(-25deg)"
   },
   {
@@ -77,8 +77,8 @@ var polygons = [
     //polygon: "50% 0%, 0% 100%, 100% 100%",
     polygon: "0% 0%, 100% 0%, 100% 100%, 100% 100%, 0% 100%, 0% 100%",
     background: "135deg, #FFE141 0%, #FFE141 25%, #FFE141 77%, #FFE141 100%",
-    width: "84",
-    height: "84",
+    width: "28",
+    height: "28",
     transform: "rotate(0deg)"
   },
   {
@@ -86,16 +86,16 @@ var polygons = [
     //polygon: "50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%",
     polygon: "50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%, 0% 38%",
     background: "135deg, #FF7373 0%, #FF7373 11%, #FF7373 79%, #FF7373 100%",
-    width: "105",
-    height: "100",
+    width: "35",
+    height: "33",
     transform: "rotate(45deg)"
   },
   {
     type: "hexagon",
     polygon: "50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%",
     background: "135deg, #569CF8 0%, #569CF8 5%, #569CF8 66%, #569CF8 100%",
-    width: "98",
-    height: "112",
+    width: "32",
+    height: "37",
     transform: "rotate(130deg)"
   }
 ]
